3ac39bc109a92b3a0f621c08b52fd0d17a68ecf2
thanhdevapp <thanhdevapp@gmail.com>
2016-02-14 23:25:01 +0700

    Initial project

---
+++ COMP1640_CW/COMP1640_CW/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(COMP1640_CW.Startup))]
+namespace COMP1640_CW
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
