101f5f7116d39ee10085b4af675c6c60715386cb
guiltygearxx <guiltygearxx.94@gmail.com>
2016-03-05 00:52:53 +0700

    Create Course, Classes ,Íntrutors

---
+++ COMP1640_CW/CMSCMV5/Server.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CMSCMV5
+{
+    internal class Server
+    {
+        internal static void ClearError()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static Exception GetLastError()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
